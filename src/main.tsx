51cb24c5cf6e2a1a17d555ff9329971718ec2cb2
Mustafa Dalga <mustafadalgaa@gmail.com>
2023-06-20 01:55:38 +0300

    required libraries have been installed

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx';
+import "xterm/css/xterm.css"
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import "@/assets/css/app.css"
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
